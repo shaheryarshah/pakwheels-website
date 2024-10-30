d7f461e04c5cc40d1cd7eb9a224499bf94dde95a
shaheryarshah <160542494+shaheryarshah@users.noreply.github.com>
2024-10-30 16:18:51 +0500

    Update navbar.tsx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,15 +2,21 @@ import Link from "next/link"
 export default function Navbar() {
   return (<nav>
     <div>
-      <ul className="flex item-center text-center justify-center text-white font-bold gap-80 border-b-2 ">
-        
-        <li><Link href="Download App via SMS">Download App via SMS</Link></li>
-        <li><Link href="Urdu">Urdu</Link></li>
+    <div className="flex  items-left ml-80 justify-left text-white font-bold">
+        <ul className="flex items-center gap-2 text-center justify-center text-white font-bold ">
+        <li className="w-3 h-3 flex flex-col"> <img src="https://pakwheel-clone-mtk.vercel.app/images/mobile.png" alt="loading" /></li>
+        <li className="flex flex-row items-left mt-3"><Link href="Download App via SMS">Download App via SMS</Link></li>
+        </ul>
+        </div>
+        <div>
+        <ul className="flex items-end justify-end  font-bold gap-10 mr-80  ">
+        <li className="text-red-500"><Link href="Urdu">اردو</Link></li>
         <li><Link href="Sign Up">Sign Up</Link></li>
         <li><Link href="Sign In">Sign In</Link></li></ul>
-
+        </div>
+        <div>
       <ul className="text-white font-bold flex justify-center gap-20 ">
-        <span><img className="w-60 h-15" src="https://static.pakwheels.com/2016/09/PakWheels-Logo-Blue-Tagline.png" alt="loading" /></span>
+        <span><img className="w-40 h-15"  src="https://static.pakwheels.com/2016/09/PakWheels-Logo-Blue-Tagline.png" alt="loading" /></span>
         <li>Used Cars</li>
         <li>New Cars</li>
         <li>Bikes</li>
@@ -22,7 +28,7 @@ export default function Navbar() {
       </ul></div>
       <div>
       <img className="w-screen h-80" src="https://cache2.pakwheels.com/system/super_boards/background_images/000/000/008/original/8.jpeg?1729810786" alt="loading" />
-      </div>
+      </div></div>
   </nav>
   );
 }
